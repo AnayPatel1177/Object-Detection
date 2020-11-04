e6945d81f9aa857df225aaa0af000cf745006793
AnayPatel1177 <72863688+AnayPatel1177@users.noreply.github.com>
2020-11-04 19:51:41 +0530

    Add files via upload

---
+++ book.js
@@ -0,0 +1,16 @@
+img = "";
+status = "";
+function preload() {
+    img = loadImage('dog_cat.jpg')
+}
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocssd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects"
+}
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
